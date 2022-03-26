087c9dc1eda7a943ca8d095c07898f7ad06e8543
Joan Jaume Oliver <joanjaumeoliver@gmail.com>
2022-03-26 20:14:30 +0100

    Modified Iterator, dimensions to use, gestures to use, one out method for test/train datasets and created sequences of 10 frames.

--- A3TGCN2_IRIGesture.py
+++ A3TGCN2_IRIGesture.py
@@ -8,6 +8,9 @@ import torch
 import torch.nn.functional as F
 from pathlib import Path
 from torch_geometric_temporal.nn.recurrent import A3TGCN2
+from torch.utils.tensorboard import SummaryWriter
+
+from dataset.IRIDatasetTemporal import IRIGestureTemporal
 
 seed = 1997
 np.random.seed(seed)
@@ -17,119 +20,121 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = True
 
 DEVICE = torch.device('cpu')
-shuffle=True
+shuffle = True
 batch_size = 32
 
-from dataset.IRIDatasetTemporal import IRIGestureTemporal
-loader = IRIGestureTemporal(os.path.join(Path().absolute(),'dataset'), 'Dynamic')
-dataset = loader.getDynamicTemporalDataset()
+loader = IRIGestureTemporal(os.path.join(Path().absolute(), 'dataset'))
+dataset = loader.getAllDataset()
+train_dataset, test_dataset = loader.getDataset()
 # n frames
 # 33 nodes
 # 4 features
 # 8 outputs
 print("Dataset type:  ", dataset)
-print("Number of samples / sequences: ",  len(set(dataset)))
-
-# Train test split 
-from torch_geometric_temporal.signal import temporal_signal_split
-train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.8)
+print("Number of samples / sequences: ", len(set(dataset)))
 
 print("Number of train buckets: ", len(set(train_dataset)))
 print("Number of test buckets: ", len(set(test_dataset)))
 
-
 # Creating Dataloaders
-train_input = np.array(train_dataset.features) # (11562, 3, 21, 16) (192, 4, 33, 18)
-train_input = np.transpose(train_input, (0,2,1,3)) # (11562, 21, 3, 16) (192, 33, 4, 18)
-train_target = np.array(train_dataset.targets) # (11562, 16, 6) (192, 18, 7)
-train_target = np.transpose(train_target, (0,2,1)) # (11562, 6, 16) (192, 7, 18)
+train_input = np.array(train_dataset.features)  # (1669, 4, 15, 10)
+train_input = np.transpose(train_input, (0, 2, 1, 3))  # (1669, 15, 4, 10)
+train_target = np.array(train_dataset.targets)  # (1669, 10, 8)
+train_target = np.transpose(train_target, (0, 2, 1))  # (1669, 8, 10)
 train_target = np.argmax(train_target, axis=1)
-train_x_tensor = torch.from_numpy(train_input).type(torch.FloatTensor).to(DEVICE)  # (L=11562, N=21, F=3, T=16)
-train_target_tensor = torch.from_numpy(train_target).type(torch.FloatTensor).to(DEVICE)  # (L=11562, C=6, T=16)
+train_x_tensor = torch.from_numpy(train_input).type(torch.FloatTensor).to(DEVICE)  # (L=1669, N=15, F=4, T=10)
+train_target_tensor = torch.from_numpy(train_target).type(torch.FloatTensor).to(DEVICE)  # (L=1669, T=10)
 train_dataset_new = torch.utils.data.TensorDataset(train_x_tensor, train_target_tensor)
-train_loader = torch.utils.data.DataLoader(train_dataset_new, batch_size=batch_size, shuffle=shuffle,drop_last=True)
+train_loader = torch.utils.data.DataLoader(train_dataset_new, batch_size=batch_size, shuffle=shuffle, drop_last=True)
 
-
-test_input = np.array(test_dataset.features) # (2891, 3, 21, 16)
-test_input = np.transpose(test_input, (0,2,1,3)) # (11562, 21, 3, 16)
-test_target = np.array(test_dataset.targets) # (2891, 16, 6)
-test_target = np.transpose(test_target, (0,2,1)) # (11562, 6, 16)
+test_input = np.array(test_dataset.features)  # (425, 4, 15, 10)
+test_input = np.transpose(test_input, (0, 2, 1, 3))  # (425, 15, 4, 10)
+test_target = np.array(test_dataset.targets)  # (425, 10, 8)
+test_target = np.transpose(test_target, (0, 2, 1))  # (425, 8, 10)
 test_target = np.argmax(test_target, axis=1)
-test_x_tensor = torch.from_numpy(test_input).type(torch.FloatTensor).to(DEVICE)  # (B=2891, N=21, F=3, T=16)
-test_target_tensor = torch.from_numpy(test_target).type(torch.FloatTensor).to(DEVICE)  # (B=2891, C=6, T=16)
+test_x_tensor = torch.from_numpy(test_input).type(torch.FloatTensor).to(DEVICE)  # (B=425, N=15, F=4, T=10)
+test_target_tensor = torch.from_numpy(test_target).type(torch.FloatTensor).to(DEVICE)  # (B=425, T=10)
 test_dataset_new = torch.utils.data.TensorDataset(test_x_tensor, test_target_tensor)
-test_loader = torch.utils.data.DataLoader(test_dataset_new, batch_size=batch_size, shuffle=shuffle,drop_last=True)
+test_loader = torch.utils.data.DataLoader(test_dataset_new, batch_size=batch_size, shuffle=shuffle, drop_last=True)
 
 
 # Making the model 
 class AttentionGAT(torch.nn.Module):
-    def __init__(self, node_features, periods, batch_size):
+    def __init__(self, node_features, number_nodes, number_targets, periods, batch_size):
         super(AttentionGAT, self).__init__()
         # Attention Temporal Graph Convolutional Cell
-        self.tgnn = A3TGCN2(in_channels=node_features,  out_channels=32, periods=periods,batch_size=batch_size) # node_features=21, periods=16
+        self.tgnn = A3TGCN2(in_channels=node_features, out_channels=32, periods=periods,
+                            batch_size=batch_size)  # node_features=21, periods=16
         # Equals single-shot prediction
         self.linear = torch.nn.Linear(32, periods)
         self.softmax = torch.nn.Softmax()
-        self.linear2 = torch.nn.Linear(33, 7)
+        self.linear2 = torch.nn.Linear(number_nodes, number_targets)
 
     def forward(self, x, edge_index):
         """
         x = Node features for T time steps
         edge_index = Graph edge indices
         """
-        h = self.tgnn(x, edge_index) # x [b=32, 21, 3, 16]  returns h [b=32, 21, 32]
-        h = F.relu(h) 
-        h = self.linear(h) #returns h [b=32, 21, 16]
+        h = self.tgnn(x, edge_index)  # x [b=32, 21, 3, 16]  returns h [b=32, 21, 32]
+        h = F.relu(h)
+        h = self.linear(h)  # Returns h [b=32, 21, 16]
         h = torch.transpose(h, 2, 1)
         h = self.linear2(h)
         h = torch.transpose(h, 2, 1)
-        #h = self.softmax(h) #Implicit in CrossEntropyLoss
-        #target (b=32, C=6, F=16)
+        # h = self.softmax(h) # Implicit in CrossEntropyLoss
+        # target (b=32, C=6, F=16)
         return h
 
-# Create model and optimizers
-model = AttentionGAT(node_features=4, periods=18, batch_size=batch_size).to(DEVICE)
 
-#model.load_state_dict(torch.load("C:\_Projects\IRIGesture\A3TGCN2_Checkpoints\Epoch_29.pth"))
+# Create model and optimizers
+model = AttentionGAT(node_features=4, number_nodes=loader.number_nodes, number_targets=loader.number_targets,
+                     periods=loader.number_frames, batch_size=batch_size).to(DEVICE)
+
+# model.load_state_dict(torch.load("C:\_Projects\IRIGesture\A3TGCN2_Checkpoints\Epoch_29.pth"))
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 loss_fn = torch.nn.CrossEntropyLoss()
 
 # Loading the graph once because it's a static graph
+static_edge_index = 0
 for snapshot in train_dataset:
     static_edge_index = snapshot.edge_index.to(DEVICE)
-    break;
+    break
 
 # Training the model 
 model.train()
 
-for epoch in range(300):#(30, 60):
+writer = SummaryWriter()
+
+for epoch in range(300):  # (30, 60):
     step = 0
     loss_list = []
     acc_list = []
     for encoder_inputs, labels in train_loader:
-        y_hat = model(encoder_inputs, static_edge_index)         # Get model predictions
-        #labels -> [32, 16]
-        #y_hat -> [32, 6, 16]
-        loss = loss_fn(y_hat.float(), labels.long()) 
+        y_hat = model(encoder_inputs, static_edge_index)  # Get model predictions
+        # labels -> [32, 16]
+        # y_hat -> [32, 6, 16]
+        loss = loss_fn(y_hat.float(), labels.long())
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        step= step+ 1
+        step = step + 1
         loss_list.append(loss.item())
-        
-        corrects = torch.flatten((torch.argmax(y_hat, dim = 1) == labels).float())
-        acc = corrects.sum()/len(corrects)
+
+        corrects = torch.flatten((torch.argmax(y_hat, dim=1) == labels).float())
+        acc = corrects.sum() / len(corrects)
         acc_list.append(acc.numpy())
-        
-        if step % 100 == 0 :
-            print("Loss = " + str(sum(loss_list)/len(loss_list)))
-            print("Acc = " + str(sum(acc_list)/len(acc_list)))
-    print("Epoch {} train CrossEntropyLoss: {:.4f} Acc: {:.4f}".format(epoch, sum(loss_list)/len(loss_list), sum(acc_list)/len(acc_list)))
-    torch.save(model.state_dict(), "C:\_Projects\IRIGesture\A3TGCN2_Checkpoints\EpochTemporal_"+str(epoch)+".pth")
 
-
-## Evaluation
+        if step % 10 == 0:
+            print("Loss = " + str(sum(loss_list) / len(loss_list)))
+            print("Acc = " + str(sum(acc_list) / len(acc_list)))
+    print("Epoch {} train CrossEntropyLoss: {:.4f} Acc: {:.4f}".format(epoch, sum(loss_list) / len(loss_list),
+                                                                       sum(acc_list) / len(acc_list)))
+    writer.add_scalar('Loss/Train', sum(loss_list) / len(loss_list), epoch)
+    writer.add_scalar('Accuracy/Train', sum(acc_list) / len(acc_list), epoch)
+    torch.save(model.state_dict(), os.path.join(os.path.join(Path().absolute(), 'A3TGCN2_Checkpoints'),
+                                                'EpochTemporal_' + str(epoch) + '.pth'))
+# Evaluation
 
 model.eval()
 step = 0
@@ -142,9 +147,16 @@ for encoder_inputs, labels in test_loader:
     # Mean squared error
     loss = loss_fn(y_hat.float(), labels.long())
     total_loss.append(loss.item())
-    
-    corrects = torch.flatten((torch.argmax(y_hat, dim = 1) == labels).float())
-    acc = corrects.sum()/len(corrects)
+
+    corrects = torch.flatten((torch.argmax(y_hat, dim=1) == labels).float())
+    acc = corrects.sum() / len(corrects)
     total_acc.append(acc.numpy())
-    
-print("Test CrossEntropyLoss: {:.4f} Acc: {:.4f}".format(sum(total_loss)/len(total_loss), sum(total_acc)/len(total_acc)))
+
+print("Test CrossEntropyLoss: {:.4f} Acc: {:.4f}".format(sum(total_loss) / len(total_loss),
+                                                         sum(total_acc) / len(total_acc)))
+
+writer.add_scalar('Loss/Test', sum(total_loss) / len(total_loss), step)
+writer.add_scalar('Accuracy/Test', sum(total_acc) / len(total_acc), step)
+
+writer.flush()
+writer.close()
--- dataset/IRIDatasetTemporal.py
+++ dataset/IRIDatasetTemporal.py
@@ -3,6 +3,8 @@ import re
 import glob
 import base64
 import shutil
+from typing import Tuple
+
 import numpy as np
 
 # Imports needed for GitHub dataset downloading.
@@ -13,7 +15,7 @@ from github import GithubException
 import torch
 from torch_geometric.data import (InMemoryDataset, Data)
 from torch_geometric_temporal.signal import DynamicGraphTemporalSignal
-from torch_geometric_temporal.signal import StaticGraphTemporalSignal
+
 
 class IRIGestureTemporal(InMemoryDataset):
     r"""The IRIGesture dataset
@@ -21,8 +23,8 @@ class IRIGestureTemporal(InMemoryDataset):
         TO DO
     Args:
         root (string): Root directory where the dataset should be saved.
-        temporal (boolean): If set to true dataset with temporal component
             would be returned.
+        testSubject (string): Subject to exclude from Dataset in order to use as Test.
         dataTypes (string): Use 'Dynamic' or 'Static'.
         token (string, optional): GitHub token needed in order to download 
             IRIGesture dataset. (By default uses 'GITHUB_TOKEN' environment 
@@ -54,19 +56,66 @@ class IRIGestureTemporal(InMemoryDataset):
     __categoriesDynamic = ['greeting', 'continue', 'turnback', 'no', 'slowdown', 'come', 'back']
     __token = os.environ.get("GITHUB_TOKEN", None)
     __processed = False
-        
-    def __init__(self, root, dataTypes = "Static", token=None, categories=None,
+
+    __train_features = []
+    __train_targets = []
+    __test_targets = []
+    __test_features = []
+
+    __nodes_to_use = [0,  # nose
+                      # 1,       # left_eye_inner
+                      # 2,       # left_eye
+                      # 3,       # left_eye_outer
+                      # 4,       # right_eye_inner
+                      # 5,       # right_eye
+                      # 6,       # right_eye_outer
+                      # 7,       # left_ear
+                      # 8,       # right_ear
+                      # 9,       # mouth_left
+                      # 10,      # mouth_right
+                      11,  # left_shoulder
+                      12,  # right_shoulder
+                      13,  # left_elbow
+                      14,  # right_elbow
+                      15,  # left_wrist
+                      16,  # right_wrist
+                      17,  # left_pinky
+                      18,  # right_pinky
+                      19,  # left_index
+                      20,  # right_index
+                      21,  # left_thumb
+                      22,  # right_thumb
+                      23,  # left_hip
+                      24  # right_hip
+                      # 25,      # left_knee
+                      # 26,      # right_knee
+                      # 27,      # left_ankle
+                      # 28,      # right_ankle
+                      # 29,      # left_heel
+                      # 30,      # right_heel
+                      # 31,      # left_foot_index
+                      # 32]      # right_foot_index
+                      ]
+
+    number_nodes = 15
+    number_targets = -1
+    number_frames = 10
+    frames_gap = 5
+    __testSubject = 'S2'
+
+    def __init__(self, root, dataTypes="Static", testSubject="S2", token=None, categories=None,
                  transform=None, pre_transform=None, pre_filter=None):
-        
+
         if dataTypes == "Dynamic":
             self.StaticData = False
             self.DynamicData = True
         else:
             self.StaticData = True
             self.DynamicData = False
-        
+
         self.dataTypes = dataTypes
-        
+        self.__testSubject = testSubject
+
         token = self.__token if token is None else token
         self.__token = token
 
@@ -78,13 +127,20 @@ class IRIGestureTemporal(InMemoryDataset):
         else:
             if self.DynamicData:
                 self.__categories = self.__categoriesDynamic
-        
+
+        self.number_targets = len(self.__categories)
+
         super().__init__(root, transform, pre_transform, pre_filter)
-        self.data, self.slices = torch.load(self.processed_paths[0])        
-        
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
         if not self.__processed:
-            self.features =  torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_feat.pt'))
-            self.targets =  torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_trgs.pt'))
+            self.features = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_feat.pt'))
+            self.targets = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_trgs.pt'))
+            self.__test_features = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_tsfeat.pt'))
+            self.__train_features = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_trfeat.pt'))
+            self.__test_targets = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_tstrgs.pt'))
+            self.__train_targets = torch.load(os.path.join(self.processed_dir,
+                                                           f'{self.dataTypes[:3]}_trtrgs.pt'))
             self.CCO = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_CCO.pt'))
             self.__totalElements = torch.load(os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_tels.pt'))
 
@@ -95,7 +151,6 @@ class IRIGestureTemporal(InMemoryDataset):
             return ['S1_attention_1_1m_upper.npy', 'S6_stop_2_4m_full.npy']
         else:
             return ['S1_continue_3_6m_full.npy', 'S10_turnback_1_1m_upper.npy']
-
 
     @property
     def processed_file_names(self):
@@ -109,10 +164,10 @@ class IRIGestureTemporal(InMemoryDataset):
         repository = owner.get_repo(self.__repo)
         if os.path.exists(self.raw_dir): shutil.rmtree(self.raw_dir)
         os.makedirs(self.raw_dir)
-                 
+
         self.__recursiveDownload(repository, self.__serverPath, self.raw_dir)
 
-    def __recursiveDownload(self, repository, server_path, local_path, content_prefix = ""):
+    def __recursiveDownload(self, repository, server_path, local_path, content_prefix=""):
         contents = repository.get_contents(server_path)
 
         for content in contents:
@@ -127,125 +182,140 @@ class IRIGestureTemporal(InMemoryDataset):
                         file_name = content_prefix + "_" + content.name
                         file_content = repository.get_contents(path)
                         file_data = base64.b64decode(file_content.content)
-                        file_out = open(os.path.join(local_path, file_name),"wb")
+                        file_out = open(os.path.join(local_path, file_name), "wb")
                         file_out.write(file_data)
                         file_out.close()
                 except (GithubException, IOError) as exc:
-                    print('Error downloading %s: %s', content.path, exc)   
+                    print('Error downloading %s: %s', content.path, exc)
 
     def process(self):
         data_list = []
         self.features = []
         self.targets = []
-               
+        self.__test_features = []
+        self.__train_features = []
+        self.__test_targets = []
+        self.__train_targets = []
+
         # We create an extremly connected graph.
-        self.CCO = np.swapaxes([[i,j] for i in range(0, 33) for j in range(0, 33)], 0, 1)
-        
+        self.CCO = np.swapaxes([[i, j] for i in range(0, self.number_nodes) for j in range(0, self.number_nodes)], 0, 1)
+
         for gesture in self.__categories:
             paths = glob.glob(os.path.join(self.raw_dir, f'*{gesture}*.npy'))
             paths = sorted(paths, key=lambda e: (len(e), e))
 
             for path in paths:
+                is_test_subject = path.__contains__(self.__testSubject)
                 gesture_seq = np.load(path, allow_pickle=True)
-                number_of_frames = gesture_seq.shape[0]
-                number_of_frames = 18 #Temp bugfix
-                
-                x = np.empty([33,4,0])
-                for frame in range(0, number_of_frames):
-                    pose = gesture_seq[frame,][0]
-                    frame_landmark = np.empty([0,4])
 
-                    # There's 33 landmarks to take into account.
-                    for landmark in range(0,33):
-                        frame_landmark = np.append(frame_landmark, np.expand_dims([pose.landmark[landmark].x, pose.landmark[landmark].y, pose.landmark[landmark].z, pose.landmark[landmark].visibility], axis = 0), axis=0)
+                number_of_sequences = (gesture_seq.shape[0] // self.frames_gap) - 1
+                for seq in range(0, number_of_sequences):
+                    x = np.empty([self.number_nodes, 4, 0])
+                    for frame in range(seq * self.frames_gap, seq * self.frames_gap + self.number_frames):
+                        pose = gesture_seq[frame,][0]
+                        frame_landmark = np.empty([0, 4])
 
-                    x = np.append(x,  np.expand_dims(frame_landmark, axis = 2), axis = 2) #[33, 4, number_of_frames]
+                        # There's 33 landmarks in total.
+                        for landmark in range(0, 33):
+                            if self.__nodes_to_use.__contains__(landmark):
+                                frame_landmark = np.append(frame_landmark, np.expand_dims(
+                                    [pose.landmark[landmark].x, pose.landmark[landmark].y, pose.landmark[landmark].z,
+                                     pose.landmark[landmark].visibility], axis=0), axis=0)
+                        # x = [n° nodes, 4, number_of_frames]
+                        x = np.append(x, np.expand_dims(frame_landmark, axis=2), axis=2)
 
-                #x = np.swapaxes(np.swapaxes(x, 0, 2), 1, 2)
-                x = np.swapaxes(x, 0, 1)
-                self.features.append(x) #[4, 33, number_of_frames]
-                x = np.swapaxes(x, 0, 2)      
-                x = torch.tensor(x, dtype=torch.float) #[number_of_frames, 33, 4]
+                    x = np.swapaxes(x, 0, 1)
+                    self.features.append(x)  # [4, number_nodes, number_of_frames]
 
-                edge_index = torch.tensor(self.CCO) #[2, 1089]
-                y = self.__categories.index(gesture)
-                
-                target = np.zeros([number_of_frames, len(self.__categories)])
-                target[:,y] = 1                
-                self.targets.append(target)
-                
-                y = torch.tensor(y) #[1]
-                
-                data = Data(x=x, edge_index=edge_index, edge_attr=None, y=y)
-                if self.pre_filter is not None and not self.pre_filter(data):
-                  continue
-                if self.pre_transform is not None:
-                  data = self.pre_transform(data)
+                    if is_test_subject:
+                        self.__test_features.append(x)
+                    else:
+                        self.__train_features.append(x)
 
-                data_list.append(data)
+                    x = np.swapaxes(x, 0, 2)
+                    x = torch.tensor(x, dtype=torch.float)  # [number_of_frames, number_nodes, 4]
+
+                    edge_index = torch.tensor(self.CCO)  # [2, 1089]
+                    y = self.__categories.index(gesture)
+
+                    target = np.zeros([self.number_frames, len(self.__categories)])
+                    target[:, y] = 1
+                    self.targets.append(target)
+
+                    if is_test_subject:
+                        self.__test_targets.append(target)
+                    else:
+                        self.__train_targets.append(target)
+
+                    y = torch.tensor(y)  # [1]
+
+                    data = Data(x=x, edge_index=edge_index, edge_attr=None, y=y)
+                    if self.pre_filter is not None and not self.pre_filter(data):
+                        continue
+                    if self.pre_transform is not None:
+                        data = self.pre_transform(data)
+
+                    data_list.append(data)
 
         torch.save(self.collate(data_list), self.processed_paths[0])
         torch.save(self.features, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_feat.pt'))
+        torch.save(self.__test_features, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_tsfeat.pt'))
+        torch.save(self.__train_features, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_trfeat.pt'))
         torch.save(self.targets, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_trgs.pt'))
+        torch.save(self.__test_targets, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_tstrgs.pt'))
+        torch.save(self.__train_targets, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_trtrgs.pt'))
         torch.save(self.CCO, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_CCO.pt'))
-                           
+
         self.__totalElements = len(data_list)
         torch.save(self.__totalElements, os.path.join(self.processed_dir, f'{self.dataTypes[:3]}_tels.pt'))
-        
+
         self.__processed = True
-        
-    def getDynamicTemporalDataset(self) -> DynamicGraphTemporalSignal:
+
+    def getDataset(self) -> Tuple[DynamicGraphTemporalSignal, DynamicGraphTemporalSignal]:
+        """Returning the IRIGesture data iterator.
+
+        Return types:
+            * **(train_dataset, test_dataset)** *(tuple of DynamicGraphTemporalSignal)* - The IRIGestureTemporal dataset.
+        """
+
+        test_dataset = DynamicGraphTemporalSignal(
+            self._get_edges(number_elements=len(self.__test_features)),  # List of CCO [2, self.number_nodes**2]
+            self._get_edge_weights(number_elements=len(self.__test_features)),  # List of ones (self.number_nodes**2, )
+            self.__test_features,  # List each item (4, self.number_nodes, frames)
+            self.__test_targets  # List each item (frames, gestures)
+        )
+
+        train_dataset = DynamicGraphTemporalSignal(
+            self._get_edges(number_elements=len(self.__train_features)),  # List of CCO [2, self.number_nodes**2]
+            self._get_edge_weights(number_elements=len(self.__train_features)),  # List of ones (self.number_nodes**2, )
+            self.__train_features,  # List each item (4, self.number_nodes, frames)
+            self.__train_targets  # List each item (frames, gestures)
+        )
+
+        return train_dataset, test_dataset
+
+    def getAllDataset(self) -> DynamicGraphTemporalSignal:
         """Returning the IRIGesture data iterator.
 
         Args types:
             * **frames** *(int)* - The number of consecutive frames T, default 16.
         Return types:
             * **dataset** *(DynamicGraphTemporalSignal)* - The IRIGestureTemporal dataset.
-        """      
-        if self.DynamicData:
-            self._get_edges() #List of CCO [2, 1089]
-            self._get_edge_weights() #List of ones (1089, )
-            #self.features() # List each item (4, 33, frames)
-            #self.targets() #List each item (frames, gestures)
+        """
+        dataset = DynamicGraphTemporalSignal(
+            self._get_edges(),  # List of CCO [2, self.number_nodes**2]
+            self._get_edge_weights(),  # List of ones (self.number_nodes**2, )
+            self.features,  # List each item (4, self.number_nodes, frames)
+            self.targets  # List each item (frames, gestures)
+        )
+        return dataset
 
-            dataset = DynamicGraphTemporalSignal(
-                self._edges, self._edge_weights, self.features, self.targets
-            )
-            return dataset
-        else:
-            raise Exception("Please use getStaticTemporalDataset()")
-        
-    def getStaticTemporalDataset(self) -> StaticGraphTemporalSignal:
-        """Returning the IRIGesture data iterator.
+    def _get_edges(self, number_elements=None):
+        number_of_elements = self.__totalElements if number_elements is None else number_elements
+        edges = [self.CCO] * number_of_elements
+        return edges
 
-        Args types:
-            * **frames** *(int)* - The number of consecutive frames T, default 16.
-        Return types:
-            * **dataset** *(StaticGraphTemporalSignal)* - The IRIGestureTemporal dataset.
-        """      
-        if self.StaticData:
-            self._get_edges() #CCO [2, 1089]
-            self._get_edge_weights() #Ones (1089, )
-            #self.features() # List each item (4, 33, frames)
-            #self.targets() #List each item (frames, gestures)
-            
-            dataset = StaticGraphTemporalSignal(
-                self._edges, self._edge_weights, self.features, self.targets
-            )
-            return dataset
-        else:
-            raise Exception("Please use getDynamicTemporalDataset()")
-    
-    def _get_edges(self):
-        if self.DynamicData:
-            self._edges = [self.CCO] * self.__totalElements
-        else:
-            self._edges = self.CCO
-    
-    def _get_edge_weights(self):
-        ones = np.ones((1089, ))
-        
-        if self.DynamicData:
-            self._edge_weights = [ones] * self.__totalElements
-        else:
-            self._edge_weights = ones
+    def _get_edge_weights(self, number_elements=None):
+        number_of_elements = self.__totalElements if number_elements is None else number_elements
+        edge_weights = [np.ones((self.number_nodes ** 2,))] * number_of_elements
+        return edge_weights
