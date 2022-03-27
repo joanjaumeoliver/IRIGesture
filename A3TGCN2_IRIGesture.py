94bf183bad9018ce8a28804ac49f999ce37e1177
Joan Jaume Oliver <joanjaumeoliver@gmail.com>
2022-03-27 18:33:26 +0200

    Added weight indexes to A3TGCN2 model

--- A3TGCN2_IRIGesture.py
+++ A3TGCN2_IRIGesture.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn.functional as F
 from pathlib import Path
 from torch_geometric_temporal.nn.recurrent import A3TGCN2
+from torch_geometric_temporal.nn.attention import GMAN
 from torch.utils.tensorboard import SummaryWriter
 
 from dataset.IRIDatasetTemporal import IRIGestureTemporal
@@ -70,12 +71,12 @@ class AttentionGAT(torch.nn.Module):
         self.softmax = torch.nn.Softmax()
         self.linear2 = torch.nn.Linear(number_nodes, number_targets)
 
-    def forward(self, x, edge_index):
+    def forward(self, x, edge_index, edge_weight):
         """
         x = Node features for T time steps
         edge_index = Graph edge indices
         """
-        h = self.tgnn(x, edge_index)  # x [b=32, 21, 3, 16]  returns h [b=32, 21, 32]
+        h = self.tgnn(x, edge_index, edge_weight)  # x [b=32, 21, 3, 16]  returns h [b=32, 21, 32]
         h = F.relu(h)
         h = self.linear(h)  # Returns h [b=32, 21, 16]
         h = torch.transpose(h, 2, 1)
@@ -89,7 +90,7 @@ class AttentionGAT(torch.nn.Module):
 # Create model and optimizers
 model = AttentionGAT(node_features=4, number_nodes=loader.number_nodes, number_targets=loader.number_targets,
                      periods=loader.number_frames, batch_size=batch_size).to(DEVICE)
-
+# model = GMAN(L=1, K=8, d=8, num_his=12, bn_decay=0.1, steps_per_day= 288, use_bias=True, mask=False)
 # model.load_state_dict(torch.load("C:\_Projects\IRIGesture\A3TGCN2_Checkpoints\Epoch_29.pth"))
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -97,8 +98,10 @@ loss_fn = torch.nn.CrossEntropyLoss()
 
 # Loading the graph once because it's a static graph
 static_edge_index = 0
+static_weight_index = 0
 for snapshot in train_dataset:
     static_edge_index = snapshot.edge_index.to(DEVICE)
+    static_weight_index = snapshot.edge_attr.to(DEVICE)
     break
 
 # Training the model 
@@ -106,12 +109,12 @@ model.train()
 
 writer = SummaryWriter()
 
-for epoch in range(300):  # (30, 60):
+for epoch in range(3000):  # (30, 60):
     step = 0
     loss_list = []
     acc_list = []
     for encoder_inputs, labels in train_loader:
-        y_hat = model(encoder_inputs, static_edge_index)  # Get model predictions
+        y_hat = model(encoder_inputs, static_edge_index, static_weight_index)  # Get model predictions
         # labels -> [32, 16]
         # y_hat -> [32, 6, 16]
         loss = loss_fn(y_hat.float(), labels.long())
@@ -125,15 +128,15 @@ for epoch in range(300):  # (30, 60):
         acc = corrects.sum() / len(corrects)
         acc_list.append(acc.numpy())
 
-        if step % 10 == 0:
+        if step % 25 == 0:
             print("Loss = " + str(sum(loss_list) / len(loss_list)))
             print("Acc = " + str(sum(acc_list) / len(acc_list)))
     print("Epoch {} train CrossEntropyLoss: {:.4f} Acc: {:.4f}".format(epoch, sum(loss_list) / len(loss_list),
                                                                        sum(acc_list) / len(acc_list)))
     writer.add_scalar('Loss/Train', sum(loss_list) / len(loss_list), epoch)
     writer.add_scalar('Accuracy/Train', sum(acc_list) / len(acc_list), epoch)
-    torch.save(model.state_dict(), os.path.join(os.path.join(Path().absolute(), 'A3TGCN2_Checkpoints'),
-                                                'EpochTemporal_' + str(epoch) + '.pth'))
+    torch.save(model.state_dict(), os.path.join(Path().absolute(), 'checkpoints', 'A3TGCN2_Checkpoints',
+                                                'Epoch_' + str(epoch) + '.pth'))
 # Evaluation
 
 model.eval()
@@ -143,7 +146,7 @@ total_loss = []
 total_acc = []
 for encoder_inputs, labels in test_loader:
     # Get model predictions
-    y_hat = model(encoder_inputs, static_edge_index)
+    y_hat = model(encoder_inputs, static_edge_index, static_weight_index)
     # Mean squared error
     loss = loss_fn(y_hat.float(), labels.long())
     total_loss.append(loss.item())
